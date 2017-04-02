709c15faa20cde79143a2688ddf2595f6b26d7d0
agentinfraguard <agentinfraguard@gmail.com>
2017-04-02 19:57:39 +0530

    Adding user mgmt code

---
+++ src/agentController/infraGuardMain.go
@@ -0,0 +1,151 @@
+
+
+package main
+
+import (
+    
+   /* 
+    "io/ioutil"
+    "os"
+    "os/exec"
+    _ "fmt" // for unused variable issue
+    "net/smtp"
+    "log"
+    "strings"
+    "encoding/json"
+    "net/http"
+    */
+    //"agentUtil"
+    "stringUtil"
+    //"serverMgmt"
+    "fmt"
+   // "fileUtil"
+   // "userMgmt"
+    "agentUtil"
+    "userMgmt"
+    "github.com/jasonlvhit/gocron"  // go get github.com/robfig/cron  
+    //"strconv"
+)
+var freqToHitApi_InSeconds uint64 = 10
+
+
+func main() {
+    nextWork := agentUtil.GetNextWork()
+    if(nextWork != nil){
+    fmt.Println("\n\nInfraGuard.main(). Length infraGuardResponse = : ",len(nextWork)) 
+    ExecuteWork(nextWork)
+    }else{
+       fmt.Println("InfraGuard.main(). There is no new work") 
+    }
+  
+}
+
+
+
+
+
+/*func main() {
+  fmt.Println("InfraGuard.main()") 
+  respStr :=serverMgmt.DoServerRegnProcess()
+  
+  if(respStr =="0"){
+    fmt.Printf("\nServer Regn process executed successfully\n")
+    fileUtil.WriteIntoLogFile("InfraGuard.main(). Server Regn process executed successfully")
+    scheduleAgentjob()
+   
+  }else{
+    fileUtil.WriteIntoLogFile(" >>>>>>>>>> InfraGuard.main(). Abort server regn Process. >>>>> ")
+    fmt.Printf("Abort server regn Process. Chk log at /var/logs/infraguard/activityLog")
+  }
+}//main
+
+
+
+*/
+
+func scheduleAgentjob(){
+  scheduler := gocron.NewScheduler()
+  scheduler.Every(freqToHitApi_InSeconds).Seconds().Do(handleUserMgmt)
+    <- scheduler.Start()
+}
+func handleUserMgmt(){
+   var nextWork []string
+    nextWork = agentUtil.GetNextWork()
+    if(nextWork != nil){
+    fmt.Println("InfraGuard.main(). Length infraGuardResponse = : ",len(nextWork)) 
+    }else{
+       fmt.Println("InfraGuard.main(). There is no new work") 
+    }
+  
+
+}//handleUserMgmt
+
+func ExecuteWork(nextWork []string){
+  //const delim = ":"
+  var pubKey, userName, prefShell, privilege string
+  var values []string
+
+  for i := 0; i < len(nextWork); i++{
+    values = stringUtil.SplitData(nextWork[i], agentUtil.Delimiter)
+    
+    if(values[1] == "addUser"){
+       
+        values = stringUtil.SplitData(nextWork[i+1], agentUtil.Delimiter)
+        pubKey = values[1]
+
+
+        values = stringUtil.SplitData(nextWork[i+2], agentUtil.Delimiter)
+        //userName = values[1] + stringUtil.RandStringBytes(3)
+        userName = values[1]
+        
+        values = stringUtil.SplitData(nextWork[i+3], agentUtil.Delimiter)
+        prefShell = values[1]
+
+        fmt.Println("userName = : ", userName)
+        fmt.Println("Activity Name ----------- Add User -----------------------")
+        fmt.Println("pubKey = : ", pubKey)
+        fmt.Println("userName = : ", userName)
+        fmt.Println("prefShell = : ", prefShell)
+
+        status := userMgmt.AddUser(userName, prefShell, pubKey ) 
+        fmt.Println("\n247. AgentUtil.ExecuteWork(). Status Add User = : ",status)
+
+        i += 3
+
+    }
+
+    if(values[1] == "deleteUser"){
+        values = stringUtil.SplitData(nextWork[i+1], agentUtil.Delimiter)
+        userName = values[1]
+        fmt.Println("----------- userName to delete = : ", userName)
+        userName = "Vinyl94EC6C"
+        status := userMgmt.Userdel(userName, false)
+        fmt.Println("142. status deleteUser  = : ", status)
+        i += 1
+    }
+
+    if(values[1] == "changePrivilege"){
+      status := ""
+        values = stringUtil.SplitData(nextWork[i+1], agentUtil.Delimiter)
+        userName = values[1]
+
+        values = stringUtil.SplitData(nextWork[i+2], agentUtil.Delimiter)
+        privilege = values[1]
+        
+        msg := "privilege = : "+privilege +" >> userName = : "+userName
+        fmt.Println(msg)
+        if(privilege == "root"){
+           status = userMgmt.GiveRootAccess(userName)
+        }else{
+             status = userMgmt.GiveNormalAccess(userName)
+         }
+       
+        fmt.Println("142. status changePrivilege  = : ", status) //success
+        i += 2
+    }
+    
+   
+  }
+
+}
+
---
+++ src/fileUtil/FileHandlerUtil.go
@@ -0,0 +1,102 @@
+
+
+package fileUtil
+import (
+    
+    "fmt"
+    "io/ioutil"
+    "os"
+    _ "fmt" // for unused variable issue
+ 
+    "log"
+    "strings"
+
+)
+
+
+
+ const logFilePath = "/var/logs/infraguard/activityLog"   
+func IsFileExisted(filePath string) (bool) {
+   _, err := os.Stat(filePath)
+    if err != nil {
+        if os.IsNotExist(err) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/*
+   Read any type of file. If isAbortOnError = true and error occur, then
+   further execution stop. 
+
+   It returns data in String even in case of error if 'isAbortOnError' = false.
+   Note :- This method does not check whether FILE EXIST OR NOT. In that case, it may
+   also returns empty string.
+*/
+func ReadFile(filePath string, isAbortOnError bool) (string) {
+     data, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        fmt.Println("errorMsg = : ", err.Error()) 
+        if(isAbortOnError){
+            panic(err)    
+        }else{
+            return "";
+        }
+    }
+    return string(data)
+}
+
+/*
+ Below method REPLACES new contents if file already exists.
+ If 'forceCreate' is false and file does not existed beforehand, then this method
+ simply retuns to caller else file is created and data will write.
+ 
+ This method will abort if error occur while writing data.
+
+ Note :- It is up to the caller to ensure the data which is going to write is in good format and meaningful. 
+*/
+func WriteIntoFile(filePath string, dataToWrite string, forceCreate bool ){
+   var err error
+  if(IsFileExisted(filePath) == false){
+    if(forceCreate == true){
+       _, err := os.Create(filePath)
+      if err != nil {
+      errorMsg := " Error While writing into file at = : "+filePath +" Msg = : "+err.Error()
+      WriteIntoLogFile(errorMsg)
+      panic(err)
+     }
+    }else{
+      return
+    }
+ }
+ err = ioutil.WriteFile(filePath, []byte(dataToWrite),0644)
+  if err != nil {
+      errorMsg := " Error While writing into file at = : "+filePath +" Msg = : "+err.Error()
+      WriteIntoLogFile(errorMsg)
+  }
+}
+
+
+func WriteIntoLogFile(msg string) {
+  //f, err := os.OpenFile(logFilePath, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+  if(IsFileExisted(logFilePath) == false){
+    fmt.Println("log file does not existed : ",logFilePath) 
+    return
+  }
+
+  f, err := os.OpenFile(logFilePath, os.O_RDWR | os.O_APPEND, 0666)
+  if err != nil {
+    fmt.Println("error opening file : ", err.Error()) 
+
+  }
+  
+  defer f.Close()
+  log.SetOutput(f)
+  msg = strings.Replace(msg, "\n","",-1)
+  log.Println(msg)
+
+
+}
+
