ae9b3c2e946061b58a2b2cceed8197be45c0cb60
agentinfraguard <agentinfraguard@gmail.com>
2017-06-05 20:06:41 +0530

    Adding comment

---
+++ scripts/agentInstaller.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+
+
+get_osflavor(){
+
+    if [[ -f "/etc/lsb-release" ]]
+        then
+            os="ubuntu"
+            fileAgentController="agent_controller_ubuntu.sh"
+        elif [[ -f "/etc/redhat-release" ]]
+        then
+            os="rpm"
+        elif [[ -f "/etc/debian_version" ]]
+        then
+            os="debian"
+        else
+            #echo "ERROR: Cannot get the system type. Aborting entire process."
+            os="unknown"
+            #exit 1
+    fi
+  
+
+
+}
+
+<<"COMMENT"
+
+start() {
+(
+
+prog=awslogs
+exec="/usr/sbin/awslogsd"
+lockfile=/var/lock/subsys/awslogs
+pidfile=/var/run/awslogs.pid
+mutexfile=/var/lock/awslogs.mutex
+
+[ -x $exec ] || exit 5
+echo -n $"Starting $prog: "
+daemon $NICELEVEL --pidfile=$pidfile --check=${prog} "nohup $exec >/dev/null 2>&1 &"
+retval=$?
+echo [ $retval -eq 0 ] && touch $lockfile
+) 9>${mutexfile}
+rm -f ${mutexfile}
+}
+
+
+
+COMMENT
+
+create_InfraGuardDirectories(){
+    echo "Creating directories in /opt ..."
+    exec="mkdir -p /opt/infraguard/sbin"
+    $exec
+
+    exec="mkdir -p /opt/infraguard/etc"
+    $exec
+
+    exec="mkdir -p /var/logs/infraguard"
+    $exec
+
+    exec="touch  /var/logs/infraguard/activityLog"
+    $exec
+
+
+   
+    exec="chmod 700 -R /opt/infraguard"
+    $exec
+    exec="chown root:root /opt/infraguard"
+    $exec
+
+
+    exec="chmod 700 -R /var/logs/infraguard"
+    $exec
+    exec="chown root:root /var/logs/infraguard"
+    $exec
+
+    echo "completed Directories Creation"
+
+
+}
+
+
+#sudo chown root:root /path/to/application
+#sudo chmod 700 /path/to/application
+
+install_daemon(){
+    echo 'Attempting Daemon Installation'
+    cd /tmp
+    if [[ "$os" = "debian"  || "$os" = "ubuntu" ]]
+        then
+        wget -q https://github.com/terminalcloud/terminal-tools/raw/master/daemon_0.6.4-2_amd64.deb || exit -1
+        dpkg -i daemon_0.6.4-2_amd64.deb
+        echo "Daemon Installation Done Successfully on debian/ununtu"
+    else
+        wget -q http://libslack.org/daemon/download/daemon-0.6.4-1.x86_64.rpm || exit -1
+        rpm -i daemon-0.6.4-1.x86_64.rpm
+        echo "Daemon Installation Done Successfully on rpm"
+    fi
+}
+
+downloadFiles_FromGitHub() {
+    
+    echo "Downloading $fileAgentController  > This file will act as a process"
+    local url="wget -O /tmp/$fileAgentController https://raw.githubusercontent.com/agentinfraguard/agent/master/scripts/$fileAgentController"
+    wget $url--progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+    command="mv /tmp/$fileAgentController  /etc/init.d"
+    $command
+      
+   
+    exec="chown root:root /etc/init.d/$fileAgentController"
+    $exec
+    exec="chmod 700 /etc/init.d/$fileAgentController"
+    $exec
+
+
+    echo "create  /tmp/serverInfo.txt with following data $serverName:$projectId:$licenseKe >> It will remove after server regn."
+    echo "$serverName:$projectId:licenseKey" > /tmp/serverInfo.txt
+
+
+    echo ""
+    echo "Downloading infraGuardMain executable. It will take time. Please wait...."
+    url="wget -O /opt/infraguard/sbin/infraGuardMain https://raw.githubusercontent.com/agentinfraguard/agent/master/go/src/agentController/infraGuardMain"
+    
+    #url="wget -O /opt/infraguard/sbin/infraGuardMain https://raw.githubusercontent.com/agentinfraguard/agent/master/go/src/test/infraGuardMain"
+    wget $url--progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+    echo "infraGuardMain downloaded."
+    
+    
+    exec="chown root:root /opt/infraguard/sbin/infraGuardMain"
+    $exec
+    exec="chmod 700 /opt/infraguard/sbin/infraGuardMain"
+    $exec
+
+
+    echo ""
+    echo "Downloading property file i.e agentConstants.txt ...."
+    url="wget -O /opt/infraguard/etc/agentConstants.txt https://raw.githubusercontent.com/agentinfraguard/agent/master/go/src/agentConstants.txt"
+    wget $url--progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+    echo "agentConstants.txt downloaded."
+   
+
+    exec="chown root:root /opt/infraguard/etc/agentConstants.txt"
+    $exec
+    exec="chmod 700 /opt/infraguard/etc/agentConstants.txt"
+    $exec
+
+
+     if [[ "$os" = "debian"  || "$os" = "ubuntu" ]] ;then
+            echo " ------- going to call  update-rc.d for agent_controller.sh --------"
+            update-rc.d $fileAgentController defaults
+     else
+            echo " ------- going to call  chkconfig for agent_controller.sh --------"
+            chkconfig --add /etc/init.d/$fileAgentController
+     fi
+
+
+    export start="start"
+    export command="/etc/init.d/$fileAgentController"
+        
+    sh $command ${start}
+
+   
+    } # downloadFiles_FromGitHub
+
+
+    checkUserPrivileges(){
+        if [ `id -u` -ne 0 ] ; then
+            echo "error: requested operation requires superuser privilege"
+            exit 1
+        fi
+    }
+
+
+# Check whether agent already is running or not. If yes, then abort further process.
+
+echo "Checking whether agent already running or not."
+pId=$(ps -ef | grep 'infraGuardMain' | grep -v 'grep' | awk '{ printf $2 }')
+ 
+
+if [ "$pId" -gt 0 ] ; then
+    echo "Aghent Process id i.e [infraGuardMain] = : $pId"
+    echo "----------- Agent already running. Abort further process. ------------"
+    exit 1
+fi
+
+
+
+if [ $# -ne 3 ] ; then
+    echo "182. Insufficient arguments. Usage: $0 serverName projectId licenseKey"
+    exit 1
+fi
+
+
+
+
+ 
+
+ 
+##########################  return
+
+checkUserPrivileges
+# Read arguments, it will saved into /tmp/serverInfo.txt & then serverMgmt/ServerHandler.go will read.
+serverName=$1
+projectId=$2
+licenseKey=$3
+
+os=""
+fileAgentController="agent_controller.sh"
+get_osflavor
+#install_daemon
+echo "os found = : $os"
+create_InfraGuardDirectories
+downloadFiles_FromGitHub
+
+
+
+
+
+
